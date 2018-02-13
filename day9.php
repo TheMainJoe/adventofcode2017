ee7af444a1dd7082f73c3dc6a5d28d914d2f04a0
Joe <masango.ja@gmail.com>
2018-02-13 18:01:56 +0200

    Added commentary

--- day9.php
+++ day9.php
@@ -5,14 +5,15 @@
 	$score = 0;
 	$ignored = "";
 	$garbage = "";
-
+	//Mark as character to skip 
 	$skip = false;
 	while ( $char = fgetc($strInput) ):
+		//Check if it is part of the characters to skip
 		if( $skip === true ):
 			$skip = false;
 			continue;
 		endif;
-
+		//Add character to ignore list if its not the one we skip
 		if( $char !== '!' ):
 			$ignored .= $char;
 			continue;
@@ -24,35 +25,37 @@
 	$isgarbage = false;
 	$garbageCount = 0;
 	for ( $x = 0; $x < strlen($ignored); $x++ ):
+		//Check ignore list for closing garbage brackets
 		if ($ignored[$x] === '>'): 
 			$isgarbage = false; 
 			continue;
 		endif;
-
+		//Count number of garbage characters
 		if ( $isgarbage === true ):
 			$garbageCount++;
 			continue;
 		endif;
-
+		//Check list for opening garbage brackets
 		if ( $ignored[$x] === '<' ):
 			$isgarbage = true;
 			continue;
 		endif;
-
+		//Add valid non-garbage brackets to the string
 		$garbage .= $ignored[$x];	
 	endfor;
-
+	//Groups
 	$items = 0;
 
 	for ( $i = 0; $i < strlen($garbage); $i++ ):
+		//Check garbage for start of group
 		if ( $garbage[$i] === '{' ):
 			$items++;
 		endif;
-
+		//check garbage for end of group and add score
 		if ( $garbage[$i] === '}' ):
 			$score += $items;
 			$items--;
 		endif;
 	endfor;
-
+	//Display final score
 	echo $score;
